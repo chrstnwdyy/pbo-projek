e367df359aa429d1728f101738a9623244109510
Markus Dito <markusdito@gmail.com>
2023-05-31 14:33:44 +0700

    initial commit

---
+++ Interface/src/DataDiri.java
@@ -0,0 +1,12 @@
+public interface DataDiri {
+    void setNama(String nama);
+    String getNama();
+    void setTanggalLahir(String tgl);
+    String getTanggalLahir();
+    void setAgama(String agama);
+    String getAgama();
+    void setJenisKelamin(String jenisKelamin);
+    String getJenisKelamin();
+    void setAlamat(String alamat);
+    String getAlamat();
+}
